7f96a5e6436f0fb6b96252418041783dfe7f8fb0
Derck Prinzhorn <derckprinzhorn@gmail.com>
2025-01-18 18:23:45 +0100

    update run sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,14 +10,14 @@ res_dir=$OUTPUT_PATH
 duration=40  # 5 seconds 91 sec per 
 ddimsteps=70  # default 50 for shorter videos
 
-if [ "$1" == "256" ]; then
+if [ "$RESOLUTION" == "256" ]; then
     H=256
     FS=3  ## This model adopts frame stride=3, range recommended: 1-6 (larger value -> larger motion)
-elif [ "$1" == "512" ]; then
+elif [ "$RESOLUTION" == "512" ]; then
     H=320
     # NOTE: derck changed this to 16 FPS
     FS=16 ## This model adopts FPS=16, range recommended: 15-30 (smaller value -> larger motion)
-elif [ "$1" == "1024" ]; then
+elif [ "$RESOLUTION" == "1024" ]; then
     H=576
     FS=10 ## This model adopts FPS=10, range recommended: 15-5 (smaller value -> larger motion)
 else
@@ -26,14 +26,14 @@ else
 fi
 
 
-if [ "$1" == "256" ]; then
-CUDA_VISIBLE_DEVICES=0 python3 scripts/evaluation/inference.py \
+if [ "$RESOLUTION" == "256" ]; then
+CUDA_VISIBLE_DEVICES=0 uv run scripts/evaluation/inference.py \
 --seed ${seed} \
 --ckpt_path $ckpt \
 --config $config \
 --savedir $res_dir/$name \
 --n_samples 1 \
---bs 1 --height ${H} --width $1 \
+--bs 1 --height ${H} --width $RESOLUTION \
 --unconditional_guidance_scale 7.5 \
 --ddim_steps ${ddimsteps} \
 --ddim_eta 1.0 \
@@ -42,13 +42,13 @@ CUDA_VISIBLE_DEVICES=0 python3 scripts/evaluation/inference.py \
 --video_length ${duration} \
 --frame_stride ${FS}
 else
-CUDA_VISIBLE_DEVICES=0 python3 scripts/evaluation/inference.py \
+CUDA_VISIBLE_DEVICES=0 uv run scripts/evaluation/inference.py \
 --seed ${seed} \
 --ckpt_path $ckpt \
 --config $config \
 --savedir $res_dir/$name \
 --n_samples 1 \
---bs 1 --height ${H} --width $1 \
+--bs 1 --height ${H} --width $RESOLUTION \
 --unconditional_guidance_scale 7.5 \
 --ddim_steps ${ddimsteps} \
 --ddim_eta 1.0 \
